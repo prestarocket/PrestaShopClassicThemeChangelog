b43f2b2a0aad51d55732cdd376bfbb4ae7828bb6
Alex Sampaio <alex.sampaio1991@gmail.com>
2016-12-01 16:00:18 +0100

    script adding version of classic theme

---
+++ add_version.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Which version would you like to add?"
+read version_variable
+
+if [ `git branch --list ${version_variable}` ]
+	then
+	echo "Branch named $version_variable already exists"
+	exit 0
+
+else
+	echo "Branch named $version_variable does not exist"
+
+	git fetch origin &&	git checkout -b migrate
+
+	wget -O PrestaShop.zip https://download.prestashop.com/download/old/prestashop_$version_variable.zip && echo "Downloading files..." || echo "This version of PrestaShop ($version_variable) is not valid".
+
+	if [ ! -f PrestaShop.zip ]
+		then
+		echo "File not found!"
+		exit 1
+	else
+		echo "Creation of branch $version_variable"
+		git checkout -b $version_variable
+
+		echo "Decompression of PrestaShop.zip"
+		unzip PrestaShop.zip -d PrestaShop && rm PrestaShop.zip
+		unzip PrestaShop/prestashop.zip -d PrestaShopContent && rm -Rf PrestaShop
+
+		echo "Moving Classic Theme"
+		mv PrestaShopContent/themes/classic classic && rm -Rf PrestaShopContent
+
+		echo "Adding file to git & push"
+		git add classic/ && git commit -m "Add classic theme $version_variable" && git push origin $version_variable && git checkout dev
+
+		echo "Adding Classic Theme for $version_variable is complete!"
+	fi
+
+fi
+
+exit 0
