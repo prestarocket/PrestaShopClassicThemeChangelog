4fa377f3d22456b09f6a66a4187db59a3b4415a2
Alex Sampaio <alex.sampaio1991@gmail.com>
2016-12-01 16:00:35 +0100

    script generating changelog

---
+++ add_changelog.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "For which versions do you want to changelog?"
+
+echo "Initial version?"
+read initial_version
+if [ ! `git branch --list ${initial_version}` ]
+	then
+	echo "This version doesn't exist"
+	exit 0
+fi
+
+echo "Final version?"
+read final_version
+if [ ! `git branch --list ${final_version}` ]
+	then
+	echo "This version doesn't exist"
+	exit 0
+fi
+
+git checkout dev && git diff $initial_version $final_version > "Diff-${initial_version}_${final_version}.log"
+echo "Your changelog is here: Diff-${initial_version}_${final_version}.log"
+
+exit 0
